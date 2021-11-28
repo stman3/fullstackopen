d59e692e8a96ba3d6f32a45777646576e7cc8b9b
stman <75269916+stman3@users.noreply.github.com>
2021-11-28 19:12:10 +0300

    1.11*: unicafe

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,19 +5,55 @@ const Button =({handleClick,text})=>(
 
 )
 
+const Statistice =({clicks})=>{
+  const Total = clicks.good+clicks.bad+clicks.neutral
+  const average = clicks.good/(Total)
+  const positive = clicks.good * (100/Total)
+
+  if(Total===0){
+    return <p>No feedback given</p>
+
+  }
+  else{
+    return(  
+    <div>
+      <StatisticLine text='good' value={clicks.good}/>
+      <StatisticLine text='neutral' value={clicks.neutral}/>
+      <StatisticLine text='bad' value={clicks.bad}/>
+      <StatisticLine text='all' value={Total}/>
+      <StatisticLine text='average' value={average}/>
+      <StatisticLine text='positive' value={positive}/>
+      </div>
+      )
+
+  }
+  
+}
+
+
+const StatisticLine =({text,value})=>{
+  if(text==='positive')
+  return <p>{text} {value}%</p>
+  else
+  return <p>{text} {value}</p>
+}
+
 const App = () =>{
-  const [good,setGood] = useState(0)
-  const [neutral,setNeutral] = useState(0)
-  const [bad,setBad] = useState(0)
+  const [clicks,setclicks] = useState({
+    good:0,
+    neutral:0,
+    bad:0
+  })
+ 
 
   const handleGoodClick = ()=>{
-    setGood(good+1)
+    setclicks({...clicks,good: clicks.good+1})
   }
   const handleBadClick = ()=>{
-    setBad(bad+1)
+    setclicks({...clicks,bad:clicks.bad+1})
   }
   const handleneutalClick = ()=>{
-    setNeutral(neutral+1)
+    setclicks({...clicks,neutral:clicks.neutral+1})
   }
   
 
@@ -27,16 +63,16 @@ const App = () =>{
       <Button handleClick={handleGoodClick} text="good"/>
       <Button handleClick={handleneutalClick} text="neutral"/>
       <Button handleClick={handleBadClick} text="bad"/>
+      <Header text="statistics"/>
+      <Statistice clicks={clicks}/>
 
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+bad+neutral}</p>
-      <p>average{good/(good+bad+neutral)}</p>
-      <p>positive {good}%</p>
     </div>
   )
+}
+
+
+const Header =(props)=>{
+  return <h1>{props.text}</h1>
 }
 
 
