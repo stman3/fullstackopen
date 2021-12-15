44cf3b50dfa0cfef67c7b8a819f627fb1aa02d00
stman <75269916+stman3@users.noreply.github.com>
2021-12-15 08:17:25 +0300

    1.13* done

--- part1/src/App.js
+++ part1/src/App.js
@@ -10,19 +10,25 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
-   
   const [selected, setSelected] = useState(0)
+  const [vote,setVote]=useState([0,0,0,0,0,0,0])
+  
   const handelclick=()=>{
     const random = Math.floor(Math.random()*anecdotes.length)
     setSelected(random)
 
   }
-  
+  const handelclickVote=()=>{
+   const copy =[...vote]
+   copy[selected]+=1
+   setVote(copy)
+  }
 
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <button onClick={handelclick}>selected</button>
+      <p>{anecdotes[selected]} has vote {vote[selected]}</p>
+      <button onClick={handelclick}>next anecdotes</button>
+      <button onClick={handelclickVote}>vote</button>
     </div>
   )
 }
