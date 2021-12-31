5efae04f829c91593749e56174046e1902821847
stman <75269916+stman3@users.noreply.github.com>
2021-12-31 17:54:10 +0300

    2.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,56 +1,53 @@
-import React from 'react'
-import Course from './componet/Course'
-const App = () =>{
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson=(event)=>{
+    event.preventDefault()
+    const personObjet={
+      name:newName,
     }
-  ]
+    setPersons(persons.concat(personObjet))
+    setNewName('')
 
+  }
+  const handlePersonChange =(event)=>{
+    setNewName(event.target.value)
+  }
 
-  return courses.map(course=><Course key={course.id} course={course}/>)
-  
-  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person,i)=>
+        <Show person={person} key={i}/>
+
+        )}
+      </div>
+
+    </div>
+  )
+}
+
+const Show=(props)=>{
+  return(
+    <div>{props.person.name}</div>
+  )
 
 }
+
 
 export default App
