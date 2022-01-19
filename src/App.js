da47429ebb563988bb644fea8ddc797892b7cee0
stman <75269916+stman3@users.noreply.github.com>
2022-01-19 12:04:26 +0300

    Exercise 2.19 and 2.20*

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Filter from './componet/Filter'
 import PersonForm from './componet/PersonForm'
 import Persons from './componet/Persons'
 import personsService from './services/persons'
+import Notification from './componet/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
   const [filter, setfilter] = useState('')
+  const [message,setMessage]=useState(null)
 
 
   useEffect(() => {
@@ -22,37 +24,72 @@ const App = () => {
   console.log('render', persons.length, 'persons')
   
  const PersonToShow = persons.filter(person=>person.name.includes(filter))
-  const addPerson=(event)=>{
-    if(!(persons.some(person=>person.name===newName))){
-      event.preventDefault()
-      console.log(event.target)
-      const personObjet={
-        name:newName,
-        number:newNumber,
-        id:persons.at(-1).id+1,
-      }
-      personsService.create(personObjet)
-      .then(returnedPerson=>{
-        setPersons(persons.concat(returnedPerson))
-        setNewName('')
-        setNewNumber('')
-      })
-    }
-    else{
-      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-        const person = persons.find(p=>p.name===newName)
-        const changedPerson = {...person,number:newNumber}
-        personsService.update(changedPerson.id,changedPerson)
-        .then(returnedPerson=>{
-          setPersons(persons.map(person=>person.id!==changedPerson.id? person:returnedPerson))
+ const addPerson = (event) => {
+  event.preventDefault()
+  const person = persons.filter((person) =>
+      person.name === newName
+  )
+
+  const personToAdd = person[0]
+  const updatedPerson = { ...personToAdd, number: newNumber }
+
+  if (person.length !== 0) {
+    if (window.confirm(`${personToAdd.name} is already added to the phonebook, replace the old number with a new one ?`)) {
+      personsService
+        .update(updatedPerson.id, updatedPerson).then(returnedPerson => {
+          console.log(`${returnedPerson.name} successfully updated`)
+          setPersons(persons.map(personItem => personItem.id !== personToAdd.id ? personItem : returnedPerson))
           setNewName('')
           setNewNumber('')
+          setMessage(
+            `${updatedPerson.name} was successfully updated`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
         })
-
-
-      }
+        .catch((error) => {
+          console.log(error)
+          setPersons(persons.filter(person => person.id !== updatedPerson.id))
+          setNewName('')
+          setNewNumber('')
+          setMessage(
+            `[ERROR] ${updatedPerson.name} was already deleted from server`
+          )
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
     }
+  } else {
+      const personToAdd = {
+          name: newName,
+          number: newNumber
+        }
+        personsService
+          .create(personToAdd)
+          .then(returnedPerson => {
+            setPersons(persons.concat(returnedPerson))
+            setNewName('')
+            setNewNumber('')
+            setMessage(
+              `Added ${newName}`
+            )
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+          })
+          .catch(error => {
+            setMessage(
+              `[ERROR] ${error.response.data.error}`
+            )
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+            console.log(error.response.data)
+          })
   }
+}
   const handleNameChange =(event)=>{
     console.log(`name: ${event.target.value}`)
     setNewName(event.target.value)
@@ -77,7 +114,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-     
+      <Notification message={message}/>
       <Filter value={filter} onChange={handlefilterChange}/>
       <PersonForm onSubmit={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
